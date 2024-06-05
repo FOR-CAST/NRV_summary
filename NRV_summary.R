74c63977763932669f9aa5c307a52d32582415c0
Alex Chubaty <achubaty@for-cast.ca>
2024-06-05 08:52:22 -0600

    with prev

--- NRV_summary.R
+++ NRV_summary.R
@@ -351,7 +351,7 @@ patchMetrics <- function(sim) {
                     funList = funList,
                     .cacheExtra = fileInfo)
     lapply(names(dfl_cc), function(f) {
-      write.csv(dfl_cc[[f]], file.path(outputPath(sim), paste0(refCodeCC, "_", f, "_CC_raw.csv")), row.names = FALSE)
+      write.csv(dfl_cc[[f]], file.path(outputPath(sim), paste0(refCodeCC, "_", f, "_raw.csv")), row.names = FALSE)
     })
 
     mod[[refCodeCC]] <- summarizePatchMetrics(dfl_cc)
@@ -451,7 +451,7 @@ patchMetricsSeralBC <- function(sim) {
                     funList = funList,
                     .cacheExtra = fileInfo)
     lapply(names(dfl_cc), function(f) {
-      write.csv(dfl_cc[[f]], file.path(outputPath(sim), paste0(refCodeCC, "_", f, "_CC_raw.csv")), row.names = FALSE)
+      write.csv(dfl_cc[[f]], file.path(outputPath(sim), paste0(refCodeCC, "_", f, "_raw.csv")), row.names = FALSE)
     })
     mod[[refCodeCC]] <- summarizePatchMetricsSeral(dfl_cc)
     lapply(names(mod[[refCodeCC]]), function(f) {
